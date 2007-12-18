182cd1c39addde6682be8dc0bc1bd5c55c2f76b2
Simon Aittamaa <simait-2@student.ltu.se>
2007-12-18 14:47:52 +0100

    setup.py is done, only licensing is left before the official 1.0.0-pr1 release.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(
-	name='Simple/Serial Flasher',
-	version='0.1.0',
+	name='Simple-Flasher',
+	version='1.0.0-pr1',
 	description='A simple serial line flasher for M16C.',
 	author='Simon Aittamaa',
 	author_email='simait-2@student.ltu.se',
