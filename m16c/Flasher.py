bff35729733018dc5bdfc9cfce7857950311992d
Simon Aittamaa <simait-2@student.ltu.se>
2007-12-18 14:59:21 +0100

    Added licence notice, now ready for the 1.0.0-pr1 release!

--- m16c/Flasher.py
+++ m16c/Flasher.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
+
+#
+#    Simple Flasher is a serial line flashing application for Renesas M16C
+#    16-bit single-chip microcomputer.
+#
+#    Copyright (C) 2007  Simon Aittamaa <simait-2@student.ltu.se>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 """Serial line flasher for m16c microcontrollers."""
 
 import time
